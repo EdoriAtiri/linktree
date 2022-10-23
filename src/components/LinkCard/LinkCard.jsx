8ab4c4bd6b61a649b173b9edace673baece4f530
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-10-23 14:37:11 +0100

    Create Initial Structure for Project

---
+++ src/components/LinkCard/LinkCard.jsx
@@ -0,0 +1,11 @@
+import style from './LinkCard.module.css'
+
+function LinkCard() {
+  return (
+    <div>
+      <h1 className={style.heading}>hello world</h1>
+    </div>
+  )
+}
+
+export default LinkCard
