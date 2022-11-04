4fbf74e7edcd3562b6e0c8bdc3d47c256fad66e7
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-11-04 12:11:01 +0100

    Refactor app structure
    Pages folder was created within src dir. app.js code moved into a new component "Home" within pages/home folder.

--- src/App.js
+++ src/App.js
@@ -1,76 +1,11 @@
 import style from './App.module.css'
-import LinkCard from './components/LinkCard/LinkCard'
+import Home from './Pages/Home/Home'
 import Footer from './components/Footer/Footer'
-import user from './Assets/83.jpg'
-import arrow from './Assets/curve_arrow.svg'
-import menu from './Assets/menu.svg'
-import github from './Assets/github.svg'
-import slack from './Assets/slack.svg'
 
 function App() {
   return (
     <div className={style.app}>
-      <header className={style.header}>
-        <div className={style.user}>
-          <figure>
-            <img id="profile__img" src={user} alt="" />
-          </figure>
-          <h1>Edori Atiri</h1>
-        </div>
-        <button className={style.header_btn}>
-          <img src={arrow} className={style.forward_arrow} alt="arrow" />
-          <img src={menu} className={style.menu} alt="menu" />
-        </button>
-      </header>
-
-      <div className={style.card_container}>
-        <p id="twitter" className={style.slack_twitter}>
-          RiReuben
-        </p>
-        <p
-          id="slack"
-          className={style.slack_twitter}
-          style={{ display: 'none' }}
-        >
-          dori
-        </p>
-
-        <LinkCard
-          title="Zuri Team"
-          link="https://training.zuri.team/"
-          id="btn__zuri"
-        />
-        <LinkCard title="Zuri Books" link="http://books.zuri.team" id="books" />
-        <LinkCard
-          title="Python Books"
-          link="https://books.zuri.team/python-for-beginners?ref_id=dori"
-          id="book__python"
-        />
-        <LinkCard
-          title="Background Check for Coders"
-          link="https://background.zuri.team"
-          id="pitch"
-        />
-        <LinkCard
-          title="Design Books"
-          link="https://books.zuri.team/design-rules"
-          id="book__design"
-        />
-      </div>
-
-      <div className={style.slack_github}>
-        <a href="https://slack.com/">
-          <img src={slack} alt="slack" />
-        </a>
-        <a
-          href="https://github.com/EdoriAtiri/linktree"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={github} alt="github" />
-        </a>
-      </div>
-
+      <Home />
       <Footer />
     </div>
   )
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,81 @@
+import style from './Home.module.css'
+import LinkCard from '../../components/LinkCard/LinkCard'
+import user from '../../Assets/83.jpg'
+import arrow from '../../Assets/curve_arrow.svg'
+import menu from '../../Assets/menu.svg'
+import github from '../../Assets/github.svg'
+import slack from '../../Assets/slack.svg'
+
+function App() {
+  return (
+    <div className={style.home}>
+      <header className={style.header}>
+        <div className={style.user}>
+          <figure>
+            <img id="profile__img" src={user} alt="" />
+          </figure>
+          <h1>Edori Atiri</h1>
+        </div>
+        <button className={style.header_btn}>
+          <img src={arrow} className={style.forward_arrow} alt="arrow" />
+          <img src={menu} className={style.menu} alt="menu" />
+        </button>
+      </header>
+
+      <div className={style.card_container}>
+        <p id="twitter" className={style.slack_twitter}>
+          RiReuben
+        </p>
+        <p
+          id="slack"
+          className={style.slack_twitter}
+          style={{ display: 'none' }}
+        >
+          dori
+        </p>
+
+        <LinkCard
+          title="Zuri Team"
+          link="https://training.zuri.team/"
+          id="btn__zuri"
+        />
+        <LinkCard title="Zuri Books" link="http://books.zuri.team" id="books" />
+        <LinkCard
+          title="Python Books"
+          link="https://books.zuri.team/python-for-beginners?ref_id=dori"
+          id="book__python"
+        />
+        <LinkCard
+          title="Background Check for Coders"
+          link="https://background.zuri.team"
+          id="pitch"
+        />
+        <LinkCard
+          title="Design Books"
+          link="https://books.zuri.team/design-rules"
+          id="book__design"
+        />
+        <LinkCard
+          title="Contact Me"
+          link="https://background.zuri.team"
+          id="pitch"
+        />
+      </div>
+
+      <div className={style.slack_github}>
+        <a href="https://slack.com/">
+          <img src={slack} alt="slack" />
+        </a>
+        <a
+          href="https://github.com/EdoriAtiri/linktree"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={github} alt="github" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
