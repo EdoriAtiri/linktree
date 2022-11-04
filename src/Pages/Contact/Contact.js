6def151401b5505a35e5ca867a275257a2bb7d4e
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-11-04 14:32:27 +0100

    Style Contact Form
    Adds styles to the Contact.module.css and makes small changes to the
    Footer.module.css

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,42 +4,59 @@ import style from './Contact.module.css'
 function Contact() {
   return (
     <div className={style.contact}>
-      <form className={style.form}>
-        <div className={style.form_control}>
-          <label htmlFor="first_name">First Name</label>
-          <input type="text" id="first_name" />
-        </div>
+      <div className={style.container}>
+        <header className={style.header}>
+          <h1>Contact Message</h1>
+          <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        </header>
 
-        <div className={style.form_control}>
-          <label htmlFor="last_name">Last Name</label>
-          <input type="text" id="last_name" />
-        </div>
+        <form className={style.form}>
+          <div className={style.form_control}>
+            <label htmlFor="first_name">First Name</label>
+            <input
+              type="text"
+              id="first_name"
+              placeholder="Enter your first name"
+            />
+          </div>
 
-        <div className={style.form_control}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
-        </div>
+          <div className={style.form_control}>
+            <label htmlFor="last_name">Last Name</label>
+            <input
+              type="text"
+              id="last_name"
+              placeholder="Enter your last name"
+            />
+          </div>
 
-        <div className={style.form_control}>
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            cols="30"
-            rows="10"
-            placeholder="Send me a message and I'll reply you as soon as possible..."
-          ></textarea>{' '}
-        </div>
+          <div className={`${style.form_control} ${style.email}`}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="yourname@email.com" />
+          </div>
 
-        <div className={style.form_control}>
-          <label htmlFor="terms">
-            You agree to providing your data to Edori Atiri who may contact you.
-          </label>
-          <input type="checkbox" name="" id="terms" />
-        </div>
+          <div className={`${style.form_control} ${style.message}`}>
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+            ></textarea>{' '}
+          </div>
 
-        <button type="submit">Send Message</button>
-      </form>
+          <div className={style.form_control}>
+            <p>
+              You agree to providing your data to Edori Atiri who may contact
+              you.
+            </p>
+
+            <input type="checkbox" name="" id="terms" />
+          </div>
+
+          <button type="submit" id="btn__submit">
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
